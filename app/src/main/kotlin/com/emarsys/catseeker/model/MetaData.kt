cb4903bcc71da9668673b61b8fe441d6bfb0e128
jpollak <jozsef.pollak@emarsys.com>
2017-04-26 23:00:41 +0200

    feat(meta-data): initialize map lazily

--- app/src/main/kotlin/com/emarsys/catseeker/model/MetaData.kt
+++ app/src/main/kotlin/com/emarsys/catseeker/model/MetaData.kt
@@ -4,16 +4,15 @@ import android.content.pm.PackageManager
 import com.emarsys.catseeker.CatSeekerApp
 
 object MetaData {
-    private val map: MutableMap<String, Any> = mutableMapOf()
-    val apiSecret: String by map
-
-    init {
+    private val map: Map<String, Any> by lazy {
         val metaData = CatSeekerApp.instance
                 .packageManager
                 .getApplicationInfo(CatSeekerApp.instance.packageName, PackageManager.GET_META_DATA)
                 .metaData
-        metaData.keySet().forEach { key ->
-            map[key] = metaData.get(key)
-        }
+
+        metaData.keySet()
+                .associate { it to metaData[it] }
     }
+
+    val apiSecret: String by map
 }
